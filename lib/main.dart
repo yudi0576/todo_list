f5a521e7cf23f435acb6f5b249c41d35867307ea
hendra <st4raja@gmail.com>
2023-06-07 00:10:47 +0700

    todo list

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list/models/notes_operation.dart';
+import 'package:todo_list/screen/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override  
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<NotesOperation>(
+      create: (context) => NotesOperation(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
